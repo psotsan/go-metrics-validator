1c263984604bc360a94931600e9f4726b76fe057
Pedro <psotsan@pm.me>
2026-05-18 19:35:28 +0200

    Reading functions

--- main.go
+++ main.go
@@ -9,21 +9,21 @@ func main() {
 	const separator = ","
 
 	if len(os.Args) != 3 {
-		fmt.Println("[-] Must provide exactly 2 argument: [metrics] [thresholds]")
-		return
+		fmt.Fprintln(os.Stderr, "[-] Must provide exactly 2 argument: [metrics] [thresholds]")
+		os.Exit(1)
 	}
 
 	metricsFile, err := os.Open(os.Args[1])
 	if err != nil {
-		fmt.Printf("[-] %s metrics file could not be opened or does not exist\n", os.Args[1])
-		return
+		fmt.Fprintf(os.Stderr, "[-] %s metrics file could not be opened or does not exist\n", os.Args[1])
+		os.Exit(1)
 	}
 	defer metricsFile.Close()
 
 	thresholds, err := readThresholds(os.Args[2], separator)
 	if err != nil {
-		fmt.Printf("[-] %v\n", err)
-		return
+		fmt.Fprintf(os.Stderr, "[-] %v\n", err)
+		os.Exit(1)
 	}
 	fmt.Println(thresholds)
 }
--- validator.go
+++ validator.go
@@ -3,18 +3,20 @@ package main
 import (
 	"bufio"
 	"errors"
-	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
-func getFields(l string, n int, s string) ([]string, bool) {
+type metric struct{}
+
+func splitAndValidate(l string, n int, s string) ([]string, bool) {
 	f := strings.Split(l, s)
 	return f, len(f) == n
 }
 
-func readThresholds(fs string, s string) (map[string]int, error) {
-	thres := make(map[string]int)
+func readThresholds(fs string, s string) (map[string]float64, error) {
+	thres := make(map[string]float64)
 
 	f, err := os.Open(fs)
 	if err != nil {
@@ -24,12 +26,34 @@ func readThresholds(fs string, s string) (map[string]int, error) {
 
 	sc := bufio.NewScanner(f)
 	for sc.Scan() {
-		fields, ok := getFields(sc.Text(), 2, s)
+		fields, ok := splitAndValidate(sc.Text(), 2, s)
 		if !ok {
 			return thres, errors.New("Thresholds file: format fault")
 		}
-		fmt.Println(fields)
+
+		val, err := strconv.ParseFloat(fields[1], 64)
+		if err != nil {
+			return thres, err
+		}
+
+		if err := sc.Err(); err != nil {
+			return thres, err
+		}
+
+		thres[strings.ToLower(fields[0])] = val
 	}
 
 	return thres, nil
+}
+
+func readMetrics(fs string, s string) (metric, error) {
+	m := metric{}
+
+	f, err := os.Open(fs)
+	if err != nil {
+		return m, err
+	}
+	defer f.Close()
+
+	return m, nil
 }
